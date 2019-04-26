348ce828d7a5f5fc20d1d1917759949cda2c6279
Luis Zabala <luis.zabala076@gmail.com>
2019-04-26 16:55:22 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // your code here
+
+function getRepositories() {
+  event.preventDefault();
+  fetch('https://api.github.com/users/octocat/repos')
+  .then(res => res.json())
+  .then(data => displayRepositories(data));
+}
+
+function displayRepositories(repos) {
+  repos.forEach(r => {
+    reposEl = document.getElementById('repositories');
+    reposEL.innerHTML += 
+    <div>
+    <p>${r.name}</p>
+    <a href="${r.html_url}">${r.html_url}</a><br>
+    <a href="#" onClick="getCommits(${r.commits_url.slice(0, -6)})"</a>
+    </div>
+})
+}
+
+function getCommits(url) {
+  fetch(url).then(r => r.json()).then(data => {
+    
+  })
+}
